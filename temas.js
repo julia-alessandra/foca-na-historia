079094d4e414381835fd31951cdb3521982afcd1
Júlia Alessandra <106632907+juywwt@users.noreply.github.com>
2022-06-15 18:43:05 -0300

    Create temas.js

---
+++ temas.js
@@ -0,0 +1,9 @@
+/**Java script do menu**/
+let botaoEl = document.querySelector('#alterna-menu');
+let bodyEl = document.querySelector('body');
+
+function alternaMenu() {
+  bodyEl.classList.toggle('menu-visivel');
+}
+
+botaoEl.addEventListener('click', alternaMenu);
