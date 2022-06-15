ff52b10e6cd6f79b43bb180e744880297adf83bc
Júlia Alessandra <106632907+juywwt@users.noreply.github.com>
2022-06-15 18:44:54 -0300

    Update temas.js

--- temas.js
+++ temas.js
@@ -1,4 +1,5 @@
-/**Java script do menu**/
+/**Java script do menu
+código baseado nos slides de laboratorio de programação web: https://fegemo.github.io/cefet-front-end/classes/js2/#menu-lateral**/
 let botaoEl = document.querySelector('#alterna-menu');
 let bodyEl = document.querySelector('body');
 
