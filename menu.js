303115446fb74eb48356eb361f5311d10365ceea
Júlia Alessandra <106632907+julia-alessandra@users.noreply.github.com>
2022-07-13 19:15:41 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,3 @@
-
 /**Java script do menu**/
 let botaoEl = document.querySelector('#alterna-menu');
 let bodyEl = document.querySelector('body');
@@ -9,9 +8,11 @@ function alternaMenu() {
 
 botaoEl.addEventListener('click', alternaMenu);
 
+/**js do titulo*/
+
 let deg = 6;
 for (let i = 1; i <= 20; i++) {
-  let div = document.querySelector(".container div:nth-child(" + i + ")");
+  let div = document.querySelector("#container div:nth-child(" + i + ")");
   div.style.transform = 'rotate(' + deg + 'deg)';
   deg = deg + 6;
 }
