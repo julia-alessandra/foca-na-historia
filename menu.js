1bd4b09cc5e11c59b36a2a0c82221252e661c230
Júlia Alessandra <106632907+julia-alessandra@users.noreply.github.com>
2022-07-16 20:06:16 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,13 +1,23 @@
 /**Java script do menu**/
+
+const itensDoMenu = document.querySelectorAll('#menu-principal li');
+
+itensDoMenu.forEach(el => el.addEventListener('click', ativaItemMenu));
+
+function ativaItemMenu(e) {
+  itensDoMenu.forEach(el => el.classList.remove('menu-ativo'));
+  e.currentTarget.classList.add('menu-ativo');
+}
+
+
 let botaoEl = document.querySelector('#alterna-menu');
-let bodyEl = document.querySelector('body');
+let bodyEl = document.querySelector('nav');
 
 function alternaMenu() {
   bodyEl.classList.toggle('menu-visivel');
 }
 
 botaoEl.addEventListener('click', alternaMenu);
-
 /**js do titulo*/
 
 let deg = 6;
